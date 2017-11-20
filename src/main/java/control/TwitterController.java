829ae1df578fcb59f2f7592372eaf83c2dc1a762
mateusbandeiraa <mateusbandeiraa@gmail.com>
2017-11-20 19:04:46 -0200

    Auth fix

--- src/main/java/control/TwitterController.java
+++ src/main/java/control/TwitterController.java
@@ -8,13 +8,24 @@ import twitter4j.QueryResult;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
+import twitter4j.auth.OAuth2Token;
+import twitter4j.conf.ConfigurationBuilder;
 
 public class TwitterController {
 	
 	public static List<String> getTweetsFrom(String user) throws Exception {
 		
 		List<String> tweets = new ArrayList<>();
-		Twitter twitter = TwitterFactory.getSingleton();
+		OAuth2Token token;
+		token = getOAuth2Token();
+
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+
+		cb.setApplicationOnlyAuthEnabled(true);
+		cb.setOAuth2TokenType(token.getTokenType());
+		cb.setOAuth2AccessToken(token.getAccessToken());
+
+		Twitter twitter = new TwitterFactory(cb.build()).getInstance();
 		
 		Query query = new Query("from:" + user + " -filter:retweets AND -filter:replies AND -filter:replies");
 		query.setResultType(Query.RECENT);
@@ -28,5 +39,28 @@ public class TwitterController {
 		System.out.println(tweets.size() + " tweets retrieved.");
 		return tweets;
 
+	}
+	
+	private static OAuth2Token getOAuth2Token()
+	{
+		OAuth2Token token = null;
+		ConfigurationBuilder cb;
+
+		cb = new ConfigurationBuilder();
+		cb.setApplicationOnlyAuthEnabled(true);
+
+		try
+		{
+			token = new TwitterFactory(cb.build())
+				.getInstance().getOAuth2Token();
+		}
+		catch (Exception e)
+		{
+			System.out.println("Can't get OAuth2 token");
+			e.printStackTrace();
+			System.exit(0);
+		}
+
+		return token;
 	}
 }
